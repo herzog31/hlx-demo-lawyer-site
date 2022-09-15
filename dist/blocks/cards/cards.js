f8e6fd400c5d26158a0621a74f4fa4872001fac5
Mark J. Becker <herzog31@users.noreply.github.com>
2022-09-15 17:54:07 +0200

    Implement card block in Preact

---
+++ dist/blocks/cards/cards.js
@@ -0,0 +1,50 @@
+import { h, render } from 'https://unpkg.com/preact@latest?module';
+import { createOptimizedPicture } from '../../../scripts/scripts.js';
+
+const Card = props => {
+  const {
+    block
+  } = props;
+  const children = Array.from(block.children);
+  const content = children.map(child => {
+    const isImage = child.children.length === 1 && child.children[0].tagName === 'PICTURE';
+
+    if (isImage) {
+      child.querySelectorAll('img').forEach(img => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{
+        width: '750'
+      }])));
+      return h("div", {
+        className: "cards-card-image",
+        dangerouslySetInnerHTML: {
+          __html: child.innerHTML
+        }
+      });
+    }
+
+    return h("div", {
+      className: "cards-card-content",
+      dangerouslySetInnerHTML: {
+        __html: child.innerHTML
+      }
+    });
+  });
+  return h("li", null, content);
+};
+
+const CardList = props => {
+  const {
+    block
+  } = props;
+  return h("ul", null, Array.from(block.children).map(row => h(Card, {
+    block: row
+  })));
+};
+
+export default function decorate(block) {
+  // TODO: Put in special render method
+  const content = block.cloneNode(true);
+  block.textContent = '';
+  render(h(CardList, {
+    block: content
+  }), block);
+}
