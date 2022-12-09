e9d17762178f86f57941d3b9ff13c6ec5f746f84
Daniela Neagu <danielan@adobe.com>
2022-12-09 16:20:06 +0200

    added onActions

--- dist/blocks/product-detail-page-mfe/product-detail-page-mfe.js
+++ dist/blocks/product-detail-page-mfe/product-detail-page-mfe.js
@@ -1,9 +1,27 @@
 import { importFromStorefrontSDK } from '../../../scripts/scripts.js';
 import { renderer } from '../../common/renderer.js';
+
 export default async function decorate(block) {
   block.textContent = '';
-  const {
-    ProductDetailPage
-  } = await importFromStorefrontSDK('/catalog/containers/pdp.js');
+  const { ProductDetailPage } = await importFromStorefrontSDK('/catalog/containers/pdp.js');
+  const { onAddToCart, selectedQuantity, onUpdateWishlist, onOptionSelect } = await importFromStorefrontSDK('/catalog/api.js');
   renderer(ProductDetailPage)(block);
+
+  onAddToCart.register('onAddToCart', (data) => console.log('--FRANKLIN ADD TO CART-- ', data));
+  onUpdateWishlist.register('onUpdateWishlist', (data) => console.log('--FRANKLIN WISHLIST--', data));
+
+  onOptionSelect.register('onOptionSelect', (data) => {
+    console.log('--FRANKLIN OPTION SELECTION-- ', data);
+
+    const url = new URL(window.location.href);
+    url.searchParams.set(data.option, data.value);
+
+    window.history.replaceState(null, null, url);
+  });
+
+
+  selectedQuantity.watch((newValue) => {
+    console.log('--FRANKLIN QUANTITY UPDATED-- ', newValue);
+  });
+
 }
